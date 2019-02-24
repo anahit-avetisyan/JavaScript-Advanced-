82a2f05a64f844cf4887648677c9e15aec201a87
anahit-avetisyan <anahitavetisyan1993@gmail.com>
2019-02-24 23:16:54 +0400

    )))

--- my-react/src/components/header.js
+++ my-react/src/components/header.js
@@ -2,11 +2,12 @@ import React,{Component} from "react";
 
 
 class Header extends Component {
+    
     render(){
         return(
             <div className="divForSign">
-                <div id="signUpp" onclick="signUp()">Sign Up</div>
-                <div id="signInn" onclick="signIn()">Sign In</div>
+                <div id="signUpp" onClick={this.signUp}>Sign Up</div>
+                <div id="signInn" onClick={this.signIn}>Sign In</div>
             </div>
         )
     }
