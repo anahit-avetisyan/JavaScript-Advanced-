7e07c501ba5f63850923011bf1da33227dbb26a7
anahit-avetisyan <anahitavetisyan1993@gmail.com>
2019-03-09 20:57:13 +0400

    )))

--- my-react/public/src/App.js
+++ my-react/public/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
- import Header from "./components/header";
- import Main from "./components/main";
- import Footer from "./components/footer";
- import MainSignIn from "./components/mainSignIn";
+import Main from "./components/main";
 import './App.css';
 
 class App extends Component {
+  
   render() {
     return (
         <form id="form">
-        <Header/>
-        <MainSignIn/>
        <Main/>
-       <Footer/>
        </form>
     );
   }
--- my-react/public/src/components/main.js
+++ my-react/public/src/components/main.js
@@ -1,8 +1,16 @@
 import React,{Component} from "react";
 
 class Main extends Component {
+    signIn=()=> {
+        console.log("aaa")
+      }
     render(){
         return(
+            <React.Fragment>
+            <div className="divForSign">
+                <button id="signUpp" onClick={this.signUp}>Sign Up</button>
+                <button id="signInn" onClick={this.signIn}>Sign In</button>
+            </div>
             <div id="signUp">
                 <span id="allelem"></span>
                 <input   type="text" placeholder="Name" id="loginName" />
@@ -14,7 +22,16 @@ class Main extends Component {
                 <input   type="password" placeholder="Repeat your password" id= "RepeatPassword"/>
                 <span id = "spanOne"></span>
             </div>
-
+            <div id="divForCheckbox">
+                <input id="typeCheckbox" type="checkbox"/>
+                <label for="typeCheckbox" className="labelForCheckbox">
+                    I agree all statements in <a href="#">Terms of service</a>
+                </label>
+            </div>
+            <button id="buttonOne" type="button" onClick={this.myFunction}>CREAT ACCOUNT</button>
+            <button id="buttonTwo" type="button" onClick={this.myFunctionOne}>LOG IN</button>
+            <p id="footer">Have already an account? <b><a href="#" target = "_blank"  > Login here </a> </b></p>
+            </React.Fragment>
         )
     }
 }
