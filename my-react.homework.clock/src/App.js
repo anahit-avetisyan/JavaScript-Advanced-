67bf7c3b8da6af58b6e83d26ed563cb77b91d93f
Anahit Avetisyan <Anahit.Avetisyan@istc.com>
2019-03-14 21:17:12 +0400

    lesson lifeCicle

--- my-react.homework.clock/src/App.js
+++ my-react.homework.clock/src/App.js
@@ -2,37 +2,44 @@ import React, { Component } from 'react';
 // import Clock from './images/clo.png';
 // import Header from './component/header'
 //  import Main from './component/main'
+import Clock from './component/showClock'
 class App extends Component {
-  state = { text:" ",
-  number:0,
-class:"Green"
-  }
+state ={
+  clicked:false
+}
+deleteComponent=()=>{
+  this.setState({clicked:true})
+}
+//   state = { text:" ",
+//   number:0,
+// class:"Green"
+//   }
   
-   changeText=(e)=>{
-    this.setState({text: e.target.value});
+//    changeText=(e)=>{
+//     this.setState({text: e.target.value});
       
-   }
-   changeNumber=(e)=>{
-    this.setState({number: e.target.value});
+//    }
+//    changeNumber=(e)=>{
+//     this.setState({number: e.target.value});
     
-   }
-   showH1=()=>{
-     this.setState({class:"Yellow"})
-     console.log(this.h1)
-     console.log(this.h2)
-     console.log(this.h1)
-   }
+//    }
+//    showH1=()=>{
+//      this.setState({class:"Yellow"})
+//      console.log(this.h1)
+//      console.log(this.h2)
+//      console.log(this.h1)
+//    }
 //     state = {value: '12',
  
 // };
 
 
 //  handleChange=(event)=> {
-//   
+  
 //   this.setState({value: event.target.value});
 // }
 //  handleSubmit = (event) => {
-  //this.setState({transform:0})
+//   this.setState({transform:0})
 //   this.setState({value: event.target.value})
 // if(this.state.value>12||this.state.value<1){
 //   alert("please input right number")
@@ -50,14 +57,16 @@ class:"Green"
   
     return (
       <div className="App">
-      <input type="text" ref={el => this.text=el}  onChange={this.changeText }   name = "text" />
+       {this.state.clicked? null:<Clock/>}
+      <button onClick={this.deleteComponent}> click</button>
+      {/* <input type="text" ref={el => this.text=el}  onChange={this.changeText }   name = "text" />
       <p>{this.state.text}</p>
       <input type="number" ref={el => this.number=el} onChange={this.changeNumber }    name = "number" />
       <p>{this.state.number}</p>
       <button onClick={this.showH1} type="button">Click</button>
       <h1  className={`color${this.state.class}`} ref={el=>this.h1=el}>Hello</h1>
       <h1 ref={el=>this.h1=el}>AAAAAA</h1>
-      <h2 ref={el=>this.h2=el}>Hello</h2>
+      <h2 ref={el=>this.h2=el}>Hello</h2> */}
 
       {/* <form  >
         <input type="number"  onChange={this.handleChange } value={this.state.value} />
---
+++ my-react.homework.clock/src/component/showClock.js
@@ -0,0 +1,34 @@
+import React,{Component,Fragment} from 'react';
+class Clock extends Component{
+    state={
+        date: new Date(),
+        clicked:false
+    }
+    componentDidMount(){
+        this.time = setInterval(()=>this.changeTime(),1000)
+    }
+    changeTime=()=>{
+        this.setState({
+            date:new Date()
+        })
+    }
+    componentDidUpdate(previousProps,previousState){
+       if(previousState.date!==this.state.date){
+           console.log("jamy poxvec")
+       }
+    }
+   
+    componentWillUnmount(){
+        clearInterval(this.time);
+    }
+    render(){
+        return(
+            <Fragment>
+            <h1>Hello Anahit jan </h1>
+           {this.state.clicked? null: <p>the tim e is....{this.state.date.toLocaleTimeString()}</p>}
+           
+            </Fragment>
+        )
+    }
+}
+export default Clock;
