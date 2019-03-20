c156a1a01e76d67e29996ac464fbf306071558d1
anahit-avetisyan <anahitavetisyan1993@gmail.com>
2019-03-21 00:57:09 +0400

    Test sass))

--- my-appsass/src/App.js
+++ my-appsass/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+ 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <p>aaaaaaaaaaaaa</p>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
